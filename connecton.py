71cde6448020ef9945f90295d0ffca80648bc05e
TheDarkLord100 <miracleyt404@gmail.com>
2023-04-21 00:00:13 +0530

    Added monuments api

--- connecton.py
+++ connecton.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import face_recognition
-import cv2
+# import cv2
 import numpy as np
 import pickle
 
@@ -69,3 +69,31 @@ def addUser(name, age, face, phone):
     mydb.commit()
     mycursor.close()
     mydb.close()
+
+
+def getMonuments():
+    mydb = mysql.connector.connect(
+        host="containers-us-west-137.railway.app",
+        user="root",
+        password="KK1JadM51ULeSNrI0NG2",
+        database="railway",
+        port=6503
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute(
+        f"select * from monuments")
+    monumentList = []
+    for x in mycursor:
+        monument = {
+            "name" : x[0],
+            "details" : x[1],
+            "fees" : x[2],
+            "image" : x[3],
+            "lat" : x[4],
+            "long" : x[5]
+        }
+        monumentList.append(monument)
+    mydb.commit()
+    mycursor.close()
+    mydb.close()
+    return monumentList
